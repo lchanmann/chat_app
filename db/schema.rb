1fd4dd41d2e4310b648047385162611ce8ecd245
Chanmann Lim <chanmannlim@gmail.com>
2018-05-15 20:36:50 -0400

    [Migration]: add user_id to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_16_003319) do
+ActiveRecord::Schema.define(version: 2018_05_16_003610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,9 @@ ActiveRecord::Schema.define(version: 2018_05_16_003319) do
     t.bigint "chatroom_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 2018_05_16_003319) do
   end
 
   add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
