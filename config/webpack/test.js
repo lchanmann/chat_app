fce799df2e2b533f04a3e95fb451248b643b65fd
Chanmann Lim <chanmannlim@gmail.com>
2018-05-15 22:04:30 -0400

    configure karma for frontend tests

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -2,4 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+environment.plugins.get('Manifest').opts.writeToFileEmit = process.env.NODE_ENV !== 'test'
+environment.config.set('devtool', 'inline-source-map')
+
 module.exports = environment.toWebpackConfig()
---
+++ karma.conf.js
@@ -0,0 +1,31 @@
+const webpackConfig = require('./config/webpack/test.js')
+module.exports = function(config) {
+  config.set({
+    basePath: "",
+    frameworks: ["jasmine"],
+    plugins: [
+      "karma-jasmine",
+      "karma-webpack",
+      "karma-chrome-launcher",
+      "karma-spec-reporter",
+      "karma-sourcemap-loader"
+    ],
+    files: [
+      "spec/javascript/*.spec.js",
+      "spec/javascript/**/*.spec.js"
+    ],
+    exclude: [],
+    webpack: webpackConfig,
+    preprocessors: {
+      "spec/javascript/*.spec.js": ["webpack", "sourcemap"],
+      "spec/javascript/**/*.spec.js": ["webpack", "sourcemap"]
+    },
+    reporters: ["progress"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ["ChromeHeadless"],
+    singleRun: false
+  });
+};
